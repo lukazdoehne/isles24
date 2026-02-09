5b42158f3359c178210e01747e41b026257dbae0
Davide Bottone <d.bottone@pm.me>
2026-02-09 20:28:22 +0000

    Updated data upload script

--- upload_runs.sh
+++ upload_runs.sh
@@ -2,4 +2,4 @@
 #
 # Upload runs to remote storage
 mkdir data/runs/
-rsync -avz --progress auto3dseg-runs/ data/runs/
+rsync -avz --progress /home/renku/work/data-local/runs/ /home/renku/work/data/runs/
