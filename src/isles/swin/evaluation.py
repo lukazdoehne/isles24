9d217bbb5f21fe18dd362e9a0d9db1ccb58c76c0
Davide Bottone <d.bottone@pm.me>
2026-03-13 09:30:29 +0000

    Suppressed redundant logging

--- src/isles/swin/evaluation.py
+++ src/isles/swin/evaluation.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import re
+import logging
 
 import torch
 from tqdm import tqdm
@@ -59,6 +60,9 @@ def final_evaluation(
     pd.DataFrame
         Dataframe with per case results results.
     """
+    # Suppress redundant INFO-level logging of MONAI writing to disk.
+    logging.getLogger("monai.transforms.io.array").setLevel(logging.WARNING)
+    
     device = torch.device(config.device)
     predictor = SwinUNETRPredictor.from_checkpoint(
         checkpoint_path=checkpoint_path, device=device, final=True, **config_overrides
